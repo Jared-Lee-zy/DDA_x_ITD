5bb5160bd6a7a5b664bd74cce25767c89472b800
Jared-Lee-zy <S10267704@connect.np.edu.sg>
2025-11-11 23:53:09 +0800

    Prefab offset
    Can now adjust the spawning offset of the prefab that spawns from the image tracking

---
+++ Assets/scripts/ImageTracker.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+public class ImageTracker : MonoBehaviour
+{
+    [SerializeField]
+    private ARTrackedImageManager trackedImageManager;
+
+    [SerializeField]
+    private GameObject[] placeablePrefabs;
+
+    [SerializeField]
+    private Vector3 spawnOffset = new Vector3(0, 0, 0);
+
+    private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
+
+    private void Start()
+    {
+        if (trackedImageManager != null)
+        {
+            trackedImageManager.trackablesChanged.AddListener(OnImageChanged);
+            SetupPrefabs();
+        }
+    }
+
+    void SetupPrefabs()
+    {
+        foreach (GameObject prefab in placeablePrefabs)
+        {
+            GameObject newPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            newPrefab.name = prefab.name;
+            newPrefab.SetActive(false);
+            spawnedPrefabs.Add(prefab.name, newPrefab);
+        }
+    }
+
+    void OnImageChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
+    {
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
+        {
+            UpdateImage(trackedImage);
+        }
+
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            UpdateImage(trackedImage);
+        }
+
+        foreach (KeyValuePair<TrackableId, ARTrackedImage> lostObj in eventArgs.removed)
+        {
+            UpdateImage(lostObj.Value);
+        }
+    }
+
+    void UpdateImage(ARTrackedImage trackedImage)
+    {
+        if(trackedImage != null)
+        {
+            GameObject prefab = spawnedPrefabs[trackedImage.referenceImage.name];
+
+            if (trackedImage.trackingState == TrackingState.Limited || trackedImage.trackingState == TrackingState.None)
+            {
+                //Disable the associated content
+                spawnedPrefabs[trackedImage.referenceImage.name].SetActive(false);
+            }
+            else if (trackedImage.trackingState == TrackingState.Tracking)
+            {
+                //Enable the associated content
+                Vector3 offsetPosition = trackedImage.transform.TransformPoint(spawnOffset);
+                prefab.transform.position = offsetPosition;
+                prefab.transform.rotation = trackedImage.transform.rotation;
+                prefab.SetActive(true);
+            }
+        }
+    }
+}
